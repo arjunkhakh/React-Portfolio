c43f9245ad10ed5a3946c3f068bcde6553b48095
Arjun Singh <arjunkhakh@gmail.com>
2022-05-20 21:27:05 +0100

    creating a css file, editing my js components and creating the pages for the tabs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Portfolio from "./components/Portfolio";
+import './App.css'
 
 function App() {
   return <Portfolio />;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import Header from './Header'
 
-function Navigation() {
+function Navigation({ currentPage, handlePageChange }) {
   return (
   <>
-   <nav class="navbar navbar-dark bg-dark">
-     <li>Home</li>
-     <li>About Me</li> 
-     <li>Resume</li> 
-     <li>Contact</li>
+  {/* class="navbar navbar-dark bg-dark" */}
+   <nav className="nav nav-tabs navbar-dark bg-dark">
+     <li className="nav-item"> <a className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'} onClick={() => handlePageChange('Home')} href="#home" >Home</a> </li>
+     <li className="nav-item"> <a className={currentPage === 'About' ? 'nav-link active' : 'nav-link' } onClick={() => handlePageChange('About')}  href="#about">About Me</a>  </li> 
+     <li className="nav-item"> <a className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'} onClick={() => handlePageChange('Resume')} href="#resume">Resume</a>  </li> 
+     <li className="nav-item"> <a className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'} onClick={() => handlePageChange('Contact')} href="#contact">Contact Me</a>  </li>
   </nav>
+  <Header />
   </>
   );
 }
