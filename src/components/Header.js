909f278871827066dd94f161c0854bb9a20c2eaa
Arjun Singh <arjunkhakh@gmail.com>
2022-05-20 15:41:52 +0100

    creating my react app and starting on the HTML of the application

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+  return (
+  <>
+   <nav> 
+     <li>Home</li>
+     <li>About Me</li> 
+     <li>Resume</li> 
+     <li>Contact</li>
+  </nav>
+  </>
+  );
+}
+
+export default Header;
