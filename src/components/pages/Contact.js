447196c44ea95ba8e5defeb3e753398f68d94656
Arjun Singh <arjunkhakh@gmail.com>
2022-05-22 00:14:01 +0100

    creating a contact me page#

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,12 +1,94 @@
-import React from 'react';
+// import React from 'react';
 
-export default function Contact() {
+// export default function Contact() {
+//   return (
+//     <div>
+//         <center>
+//       <h2 id="about-me">Contact Me!</h2>
+//       <form></form>
+//     </center>
+//     </div>
+//   );
+// }
+
+
+import React, { useState } from 'react';
+import './utils/style.css';
+
+// Here we import a helper function that will check if the email is valid
+import { validateEmail } from './utils/helpers';
+
+function Contact() {
+  // Create state variables for the fields in the form
+  // We are also setting their initial values to an empty string
+  const [email, setEmail] = useState('');
+  const [userName, setUserName] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either email, username, and password
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'userName') {
+      setUserName(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    if (!validateEmail(email) || !userName) {
+      setErrorMessage('Email or Name is invalid');
+      // We want to exit out of this code block if something is wrong so that the user can correct it
+      return;
+      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
+    }
+    alert(`Thank You for your message`);
+
+    // If everything goes according to plan, we want to clear out the input after a successful registration.
+    setUserName('');
+    setEmail('');
+  };
+
   return (
     <div>
-        <center>
-      <h2 id="about-me">Contact Me!</h2>
-      <form></form>
-    </center>
+      Contact Me Here and I wil get back to you.
+      <form className="form">
+      <input
+          name="name"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Name"
+        />
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="email"
+          placeholder="Email"
+        />
+        <input
+          name="text"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Message"
+        />
+        <button type="button" onClick={handleFormSubmit}>Submit</button>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
     </div>
   );
 }
+
+export default Contact;
