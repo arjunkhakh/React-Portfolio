582c2dbde2f5e70ac7cacc43cd998b7fcfde7fff
Arjun Singh <arjunkhakh@gmail.com>
2022-05-21 23:50:36 +0100

    creating my about me page and designing on the page

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,8 @@ import React from 'react';
 
 function Header() {
   return (
-    <>
-    
-    
+    <>    
       <h1 style={{color: "white"}} className="card-title">ARJUN KHAKH</h1>
-    
-    
   </>
   );
 }
