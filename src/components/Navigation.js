4f7ee2909d43b2a8bff50d24c1398870e14e69be
Arjun Singh <arjunkhakh@gmail.com>
2022-05-23 15:10:26 +0100

    editing my navbar text

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -9,10 +9,10 @@ function Navigation({ currentPage, handlePageChange }) {
   <div id='centered'>
    <nav className="nav nav-tabs navbar-black bg-370617">
    <Header />
-     <li id='homeTab' className="nav-item"> <a className={currentPage === 'Project' ? 'nav-link active' : 'nav-link'} onClick={() => handlePageChange('Project')} href="#project" >Projects</a> </li>
-     <li className="nav-item"> <a className={currentPage === 'About' ? 'nav-link active' : 'nav-link' } onClick={() => handlePageChange('About')}  href="#about">About Me</a>  </li> 
-     <li className="nav-item"> <a className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'} onClick={() => handlePageChange('Resume')} href="#resume">Resume</a>  </li> 
-     <li className="nav-item"> <a className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'} onClick={() => handlePageChange('Contact')} href="#contact">Contact Me</a>  </li>
+     <li id='homeTab' className="nav-item"> <a className={currentPage === 'Project' ? 'nav-link active' : 'nav-link'} onClick={() => handlePageChange('Project')} href="#project" >Projects 🚀</a> </li>
+     <li className="nav-item"> <a className={currentPage === 'About' ? 'nav-link active' : 'nav-link' } onClick={() => handlePageChange('About')}  href="#about">About Me ♥</a>  </li> 
+     <li className="nav-item"> <a className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'} onClick={() => handlePageChange('Resume')} href="#resume">Resume 📝</a>  </li> 
+     <li className="nav-item"> <a className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'} onClick={() => handlePageChange('Contact')} href="#contact">Contact Me 📞</a>  </li>
   </nav>
   </div>
   
