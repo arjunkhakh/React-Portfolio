bf66d87aff0d722bb991a21e91975f83458b48b7
Arjun Singh <arjunkhakh@gmail.com>
2022-06-20 16:48:36 +0100

    adding my programming languages and tools onto my homepage

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import me from "./images/Image from iOS.jpg"
+import { FaCss3, FaGithub, FaHtml5, FaJs, FaNodeJs, FaNpm, FaReact } from 'react-icons/fa';
 
 export default function About() {
   return (
@@ -9,6 +10,14 @@ export default function About() {
       <center>
 
          <img id="profileimg" src={me} alt='Me'></img></center>
+         <h2 style={{textAlign:"center"}}>Programming Languages And Tools</h2>
+         <FaGithub className='react-icons'/> Github
+         <FaReact className='react-icons' /> React.js
+         <FaJs className='react-icons' /> JavaScript
+         <FaHtml5 className='react-icons' /> HTML5
+         <FaCss3 className='react-icons' /> CSS3
+         <FaNodeJs className='react-icons' /> Node.js
+         <FaNpm className='react-icons' /> NPM
          <p id='profileText'>
         Welcome to my Professional Portfolio! My Name is Arjun! I am a programming bootcamper at the University of Birmingham! Located in the West 
         Midlands, I find the university such a promising place for coders and learners like myself to advance into our career and be able to 
